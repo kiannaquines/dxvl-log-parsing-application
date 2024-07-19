88f6c0bde192dcc22d82c46b51e99f4fa7a3bd87
kiannaquines <kjgnaquines@gmail.com>
2024-07-19 14:44:03 +0800

    add concurrent

--- app/views.py
+++ app/views.py
@@ -3,9 +3,14 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from app.forms import *
 from app.commons.services import *
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 
 def dashboard_page_view(request):
     return render(request, 'dashboard.html')
+
+def dxvl_logs_view(request):
+    return render(request, 'dxvl_logs.html')
 
 
 
