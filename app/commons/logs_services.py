834ea828b73a23859a2a63115082a4bbf44a78d5
kiannaquines <kjgnaquines@gmail.com>
2024-07-21 23:47:06 +0800

    add re and pattern

--- app/commons/logs_services.py
+++ app/commons/logs_services.py
@@ -1,7 +1,8 @@
 import re
-from app.utils import parser
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-def parse_dxvl_logs(object,**kwargs):
+def parse_dxvl_logs(log,**kwargs):
+    batch = []
+    log_pattern = re.compile(r"(\d{2}-[A-Z][a-z]{2}-\d{4} \d{2}:\d{2}:\d{2}) (.*?) - (.*)")
     pass
