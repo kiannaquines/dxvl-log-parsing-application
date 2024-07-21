de0762b384dd4825b820bd4fd07429a6c32ed001
kiannaquines <kjgnaquines@gmail.com>
2024-07-21 23:45:49 +0800

    remove utils folder

--- app/commons/logs_services.py
+++ app/commons/logs_services.py
@@ -1,4 +1,7 @@
+import re
 from app.utils import parser
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 
 def parse_dxvl_logs(object,**kwargs):
     pass
--- app/utils/parser.py
+++
@@ -1,5 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
-
-def parser(filename):
-    pass
