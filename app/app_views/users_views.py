cdcb35ebb4cc5c339615a5709b46ecf2ca3c6a21
kiannaquines <kjgnaquines@gmail.com>
2024-07-23 19:32:40 +0800

    update TODO.md and add new task

--- app/app_views/users_views.py
+++ app/app_views/users_views.py
@@ -3,7 +3,10 @@ from app.models import DXVLUsers
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from app.commons.common_services import all_objects_only
+from django.views.generic import UpdateView,DeleteView
+from django.urls import reverse_lazy
 
+@login_required(login_url=reverse_lazy('login'))
 def users(request):
     context = {}
     users_list = all_objects_only(DXVLUsers.objects,'username','email','first_name','last_name','date_joined')
@@ -13,3 +16,10 @@ def users(request):
 
     context['users_obj'] = page_obj
     return render(request,"users.html",context)
+
+
+class EditUserView(UpdateView):
+    pass
+
+class DeleteUserView(DeleteView):
+    pass
