98662abaa338bc5291c51e48599eb40d166c7018
kiannaquines <kjgnaquines@gmail.com>
2024-07-24 16:17:25 +0800

    refactor all generate report form

--- app/app_views/advertisement_views.py
+++ app/app_views/advertisement_views.py
@@ -1,107 +1,207 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import (
+    JsonResponse,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseRedirect,
+)
 from django.contrib.auth.decorators import login_required
 from app.commons.logs_services import *
-from app.commons.common_services import all_objects_only,all_objects_only_with_order,pagination
+from app.commons.common_services import (
+    all_objects_only,
+    all_objects_only_with_order,
+    pagination,
+)
 from django.urls import reverse_lazy
 from app.models import DXVLLogs
-from django.http import HttpResponseBadRequest
-from app.commons.generate_report_services import generate_monthly_report, generate_weekly_report, generate_daily_report
+from app.commons.generate_report_services import (
+    generate_monthly_report,
+    generate_weekly_report,
+    generate_daily_report,
+)
+from django.contrib import messages
+from datetime import datetime
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def parse_logs_view(request):
     if request.method == "POST":
         files = request.FILES.items()
-        result = parse_dxvl_logs(user=request.user.username,log_files=files)
+        result = parse_dxvl_logs(user=request.user.username, log_files=files)
 
-        if result == 'file_exists':
+        if result == "file_exists":
 
-            return JsonResponse({
+            return JsonResponse(
+                {
                     "type": result,
-                    "message": "Logs with the same name already exist or already process by the system."
-            })
-        
-        elif result == 'file_error':
+                    "message": "Logs with the same name already exist or already process by the system.",
+                }
+            )
 
-            return JsonResponse({
-                "type": result,
-                "message": "Your logs seems to have a problem or malformed. Please check and try again."
-            })
-        
-        elif result == 'success':
-            return JsonResponse({
-                "type": result,
-                "message": "Your logs have been successfully parsed. Please check the logs entry for results."
-            })
-        
+        elif result == "file_error":
+
+            return JsonResponse(
+                {
+                    "type": result,
+                    "message": "Your logs seems to have a problem or malformed. Please check and try again.",
+                }
+            )
+
+        elif result == "success":
+            return JsonResponse(
+                {
+                    "type": result,
+                    "message": "Your logs have been successfully parsed. Please check the logs entry for results.",
+                }
+            )
+
         else:
 
-            return JsonResponse({
-                "message": "An error occurred while parsing your logs. Please try again later."
-            })
-        
-    return JsonResponse({
-        "message": "An error occurred while processing your request. Please try again later."
-    })
-        
-@login_required(login_url=reverse_lazy('login'))
+            return JsonResponse(
+                {
+                    "message": "An error occurred while parsing your logs. Please try again later."
+                }
+            )
+
+    return JsonResponse(
+        {
+            "message": "An error occurred while processing your request. Please try again later."
+        }
+    )
+
+
+@login_required(login_url=reverse_lazy("login"))
 def upload_advertisement_logs(request):
-    return render(request,"upload.html")
+    return render(request, "upload.html")
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def advertisement_pricing(request):
-    return render(request,"pricing.html")
+    return render(request, "pricing.html")
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def dxvl_logs_view(request):
-    context   = {}
-    dxvl_logs = all_objects_only_with_order(DXVLLogs.objects,'date_aired','artist','advertisement','status','date_added')
-    page_obj  = pagination(dxvl_logs,request.GET.get('page'),50)
-    context['page_object'] = page_obj
-    return render(request, 'dxvl_logs.html',context)
+    context = {}
+    dxvl_logs = all_objects_only_with_order(
+        DXVLLogs.objects,
+        "date_aired",
+        "artist",
+        "advertisement",
+        "status",
+        "date_added",
+    )
+    page_obj = pagination(dxvl_logs, request.GET.get("page"), 50)
+    context["page_object"] = page_obj
+    return render(request, "dxvl_logs.html", context)
 
 
-@login_required(login_url=reverse_lazy('login'))
+@login_required(login_url=reverse_lazy("login"))
 def dxvl_daily_report_view(request):
-    context   = {}
-    dxvl_logs = all_objects_only(DXVLLogs.objects,'date_aired','artist','advertisement','status','date_added')
-    page_obj  = pagination(dxvl_logs,request.GET.get('page'),50)
-    context['page_object'] = page_obj
-    return render(request, 'daily.html',context)
+    context = {}
+    dxvl_logs = all_objects_only(
+        DXVLLogs.objects,
+        "date_aired",
+        "artist",
+        "advertisement",
+        "status",
+        "date_added",
+    )
+    page_obj = pagination(dxvl_logs, request.GET.get("page"), 50)
+    context["page_object"] = page_obj
+    return render(request, "daily.html", context)
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def dxvl_weekly_report_view(request):
-    context   = {}
-    dxvl_logs = all_objects_only(DXVLLogs.objects,'date_aired','artist','advertisement','status','date_added')
-    page_obj  = pagination(dxvl_logs,request.GET.get('page'),50)
-    context['page_object'] = page_obj
-    return render(request, 'weekly.html',context)
+    context = {}
+    dxvl_logs = all_objects_only(
+        DXVLLogs.objects,
+        "date_aired",
+        "artist",
+        "advertisement",
+        "status",
+        "date_added",
+    )
+    page_obj = pagination(dxvl_logs, request.GET.get("page"), 50)
+    context["page_object"] = page_obj
+    return render(request, "weekly.html", context)
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def dxvl_monthly_report_view(request):
-    context   = {}
-    dxvl_logs = all_objects_only(DXVLLogs.objects,'date_aired','artist','advertisement','status','date_added')
-    page_obj  = pagination(dxvl_logs,request.GET.get('page'),50)
-    context['page_object'] = page_obj
-    return render(request, 'monthly.html',context)
+    context = {}
+    dxvl_logs = all_objects_only(
+        DXVLLogs.objects,
+        "date_aired",
+        "artist",
+        "advertisement",
+        "status",
+        "date_added",
+    )
+    page_obj = pagination(dxvl_logs, request.GET.get("page"), 50)
+    context["page_object"] = page_obj
+    return render(request, "monthly.html", context)
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def daily_view(request):
     if request.method == "POST":
-        result = generate_daily_report(start_date=request.POST.get('date_from'), end_date=request.POST.get('date_to'))
 
-    return HttpResponseBadRequest("Invalid request method. Only POST requests are allowed.")
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = (
+            f'attachment; filename="dxvl_daily_report-{datetime.now()}.pdf"'
+        )
 
-@login_required(login_url=reverse_lazy('login'))
+        result = generate_daily_report(
+            start_date=request.POST.get("date_from"),
+            end_date=request.POST.get("date_to"),
+            response=response,
+        )
+
+        if result == "no_logs_found":
+
+            messages.info(
+                request,
+                "Sorry, no logs found for the given date range.",
+                extra_tags="not_found",
+            )
+
+            return HttpResponseRedirect(reverse_lazy("dxvl_daily_report_view"))
+        
+        elif result == "error_in_parsing_pdf":
+
+            messages.error(
+                request,
+                "An error occurred while generating the PDF report. Please try again later.",
+                extra_tags="error_generate_pdf",
+            )
+            
+            return HttpResponseRedirect(reverse_lazy("dxvl_daily_report_view"))
+        else:
+
+            return response
+
+    return HttpResponseBadRequest(
+        "Invalid request method. Only POST requests are allowed."
+    )
+
+
+@login_required(login_url=reverse_lazy("login"))
 def weekly_view(request):
     if request.method == "POST":
         pass
 
-    return HttpResponseBadRequest("Invalid request method. Only POST requests are allowed.")
+    return HttpResponseBadRequest(
+        "Invalid request method. Only POST requests are allowed."
+    )
 
-@login_required(login_url=reverse_lazy('login'))
+
+@login_required(login_url=reverse_lazy("login"))
 def monthly_view(request):
     if request.method == "POST":
         pass
 
-    return HttpResponseBadRequest("Invalid request method. Only POST requests are allowed.")
+    return HttpResponseBadRequest(
+        "Invalid request method. Only POST requests are allowed."
+    )
--- app/commons/generate_report_services.py
+++ app/commons/generate_report_services.py
@@ -1,11 +1,42 @@
 from xhtml2pdf import pisa
 from app.models import DXVLLogs
+from app.commons.common_services import filter_objects, filter_objects_count
+from datetime import datetime
+from dxvl.settings import BASE_DIR
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+
+def generate_daily_report(start_date, end_date, response):
+    count_check = filter_objects_count(
+        DXVLLogs.objects, date_aired__range=(start_date, end_date)
+    )
+
+    template_pdf = get_template("pdf_template/template.html")
+    context = {}
+
+    if count_check == 0:
+        return "no_logs_found"
+
+    daily_logs = filter_objects(
+        DXVLLogs.objects, date_aired__range=(start_date, end_date)
+    )
+
+    context["daily_logs"] = daily_logs
+    context["generated_date"] = datetime.now().strftime("%Y-%m-%d")
+
+    rendered = template_pdf.render(context)
+    createPDF = pisa.CreatePDF(rendered, dest=response)
+
+    if createPDF.err:
+        return "error_in_parsing_pdf"
+
+    return createPDF
+
 
 def generate_monthly_report(start_month, end_month):
     pass
 
-def generate_weekly_report(start_week, end_week):
-    pass
 
-def generate_daily_report(start_date, end_date):
+def generate_weekly_report(start_week, end_week):
     pass
